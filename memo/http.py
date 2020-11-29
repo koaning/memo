bc89fc31b108a87b8378f03baa19f2e0105002b8
koaning <vincentwarmerdam@gmail.com>
2020-11-29 13:47:09 +0100

    added-post

--- memo/http.py
+++ memo/http.py
@@ -14,7 +14,8 @@ def memweb(url):
         @wraps(func)
         def wrapper(*args, **kwargs):
             result = func(*args, **kwargs)
-            _ = httpx.post(url, data={**kwargs, **result})
+            with httpx.Client() as client:
+                _ = client.post(url, data={**kwargs, **result})
             return result
         return wrapper
     return decorator
