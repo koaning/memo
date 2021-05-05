95a13a65f3f8b788f0e8f7df31eb68500256ea12
Brad <bday@instapathbio.com>
2021-05-05 15:36:20 -0500

    demo run parallel with ray

--- demo.py
+++ demo.py
@@ -18,17 +18,15 @@ def birthday_experiment(class_size, n_sim):
     return {"est_proba": proba}
 
 
-# for setting in grid(class_size=range(20, 30), n_sim=[100, 10_000, 1_000_000]):
-#     birthday_experiment(**setting)
+for setting in grid(class_size=range(20, 30), n_sim=[100, 10_000, 1_000_000]):
+    birthday_experiment(**setting)
 
 
 # To Run in parallel
 
+data = []
 ray.init(address='auto', _redis_password='5241590000000000')
 
 settings = list(grid(class_size=range(20, 30), n_sim=[100, 10_000, 1_000_000], progbar=False))
 runner = Runner(backend="ray", n_jobs=-1)
 runner.run(func=birthday_experiment, settings=settings)
-
-print(data)
-print(len(data))
